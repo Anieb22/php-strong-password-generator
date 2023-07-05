6214fcd6723c8f619121a057f069fbba72f58ff3
Anieb22 <anielloverdino@gmail.com>
2023-07-05 17:57:43 +0200

    definita la parte grafica del form

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password Generator</title>
+</head>
+<body style="background-color: #3282F6">
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <h1 class="text-light my-2">STRONG PASSWORD GENERATOR</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 bg-light rounded p-3 d-flex flex-row flex-wrap">
+                    <div class="col-6">
+                    <h5>Numero di caratteri</h5>
+                    </div>
+                    <div class="col-6">
+                    <form action="index.php" method="post">
+                        <input type="text" name="" id="" class="w-100">
+                    </form>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <button type="submit" class="btn btn-primary mt-4">Invia</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
